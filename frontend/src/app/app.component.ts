230358f03a6d45f0522d0a64acbd4e124dc669c4
Manish Dixit <manish.dixit96@gmail.com>
2023-12-08 12:24:52 +0530

    Implemented PrimeNg UI Library

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  products: any=  [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+      this.products = [
+        {"code": 1,"name": "Bamboo Watch", "category": "Accessory", "quantity": 12},
+        {"code": 2,"name": "Black Watch", "category": "Accessory", "quantity": 34},
+        {"code": 3,"name": "Blue Band", "category": "Clothing", "quantity": 64},
+        {"code": 4,"name": "Blue T-Shirt", "category": "Fitness", "quantity": 6},
+        {"code": 5,"name": "Bracelet", "category": "Accessory", "quantity": 25}
+      ];
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
